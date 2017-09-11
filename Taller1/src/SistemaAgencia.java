99a8bfdec2396a2ad5eda232aa9dd20c0147f545
Luis Alvarado <luis.alvaradofcb@outlook.es>
2017-09-11 19:22:54 -0300

    FIX metodo login() se valida el usuario

--- Taller1/src/SistemaAgencia.java
+++ Taller1/src/SistemaAgencia.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 /**
  *
  * @author luisalvarado
@@ -7,48 +8,93 @@ import java.util.Scanner;
 public class SistemaAgencia {
   //Declaracion de variables globales
 
+   String crearUsuarioLogin,crearContraseñaLogin,usuarioLogin, contraseñaLogin,
+    ingresaUsuarioLogin, ingresaContraseñaLogin, nuevoIntento;
+   String nuevoIntento1 = "s";
+  
 
   //Método para crear usuario e ingresar al sistema
-  void login(){
+
+   void login(){
     //Declaración de variables locales del método
-    String crearUsuarioLogin,crearContraseñaLogin,usuarioLogin, contraseñaLogin,
-     ingresaUsuarioLogin, ingresaContraseñaLogin;
+        int opcionLogin;
 
-    int opcionLogin;
+        Scanner scMenuLogin = new Scanner(System.in);
+        System.out.println("------------------ Menú /login ------------------");
+        System.out.println("Por favor eliga una opción");
+        System.out.println("\t (1) Ingresar al sistema");
+        System.out.println("\t (2) Crear nuevo usuario");
+        System.out.print("Opción ingresada : ");
+        opcionLogin = scMenuLogin.nextInt();
 
-    Scanner scLogin = new Scanner(System.in);
-    System.out.println("------------------ Menú /login ------------------");
-    System.out.println("Por favor eliga una opción");
-    System.out.println("\t (1) Ingresar al sistema");
-    System.out.println("\t (2) Crear nuevo usuario");
-    System.out.print("Opción ingresada : ");
-    opcionLogin = scLogin.nextInt();
     //Validación de la opción ingresada
-    if ((opcionLogin != 1) && (opcionLogin != 2)) {
-      System.out.println("¡Ingrese una opción válida!"); //Alerta
-    }else {
-      if (opcionLogin == 2) {
-        System.out.println("------------------ Crear nuevo usuario /login ------------------");
-        System.out.println("Por favor completar con los datos solicitados");
-        System.out.println("Usuario : ");
-        crearUsuarioLogin = scLogin.nextLine();
-        System.out.println("Contraseña : ");
-        crearContraseñaLogin = scLogin.nextLine();
-        System.out.println("Su registro está completo, ahora puede ingresar al sistema");
-      }
-      if (opcionLogin == 1) {
-        System.out.println("------------------ Ingresar al sistema /login ------------------");
-        System.out.println("Para ingresar al sistema ingrese su usuario y contraseña");
-        System.out.println("Usuario : ");
-        ingresaUsuarioLogin =scLogin.nextLine();
-        System.out.println("Contraseña : ");
-        ingresaContraseñaLogin = scLogin.nextLine();
-      } /*Cierre de if*/
-    } /*Cierre de else login*/
+        if ((opcionLogin != 1) && (opcionLogin != 2)) {
+            System.out.println("¡Ingrese una opción válida!"); //Alerta
+        }else {
+            if (opcionLogin == 2) {
+                Scanner scCrearUsuario = new Scanner(System.in);
+                System.out.println("------------------ Crear nuevo usuario /login ------------------");
+                System.out.println("Por favor completar con los datos solicitados");
+                System.out.print("Usuario : ");
+                crearUsuarioLogin = scCrearUsuario.nextLine();
+                System.out.print("Contraseña : ");
+                crearContraseñaLogin = scCrearUsuario.nextLine();
+                System.out.println("Su registro está completo, ahora puede ingresar al sistema");
+            }
+            if (opcionLogin == 1) {
+                Scanner scLogin = new Scanner(System.in);
+                    for (int i = 0; i < 3; i++) {
+                        System.out.println("------------------ Ingresar al sistema /login ------------------");
+                        System.out.println("Para ingresar al sistema ingrese su usuario y contraseña");
+                        System.out.print("Usuario : ");
+                        ingresaUsuarioLogin =scLogin.nextLine();
+                        System.out.print("Contraseña : ");
+                        ingresaContraseñaLogin = scLogin.nextLine();
+                
+                        if (ingresaUsuarioLogin.equals(crearUsuarioLogin) && (ingresaContraseñaLogin.equals(crearContraseñaLogin))){
+                            System.out.println("Usuario y contraseña correcta");
+                            break;
+                    
+                        }else{
+           
+                            System.out.println("Usuario o contraseña inválido !");
+                            System.out.println("Intentelo nuevamente");
+                       
+                        }
+                    }  /*Cierre ciclo for repeticion de validacion*/ 
+                   
+            } /*Cierre de if*/
+        } /*Cierre de else login*/
 
-  } /*Cierre del método login*/
-  public static void main(String[] args) {
-    SistemaAgencia objeto1 = new SistemaAgencia();
-    objeto1.login();   
- }
+    } /*Cierre del método login*/
+   
+  
+  void menuPrincipal(){
+      System.out.println("------------------ Menú Principal ------------------");
+
+  }
+
+   
+   void imprimir(){
+        System.out.println("Usuario creado : "+ crearUsuarioLogin + "\t " +"Usuario ingresado : "  + ingresaUsuarioLogin);
+        System.out.println("Contraseña creada : "+ crearContraseñaLogin + "\t "+ "Contraseña ingresada : " + ingresaContraseñaLogin);
+    }
+
+
+    public static void main(String[] args) {
+        SistemaAgencia objeto1 = new SistemaAgencia();
+        objeto1.login();
+        objeto1.login();
+   
+        objeto1.imprimir();
+        objeto1.menuPrincipal();
+     
+        
+      
+        
+        
+        
+   
+
+    } /*Cierre metodo main*/
 } /* Cierre de la clase */
